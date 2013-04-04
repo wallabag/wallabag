1f1a6157aa480553afba0e1ed2537fff94dc7a6b
Fatih Orhan <fatihorhan@gmail.com>
2013-04-04 11:26:50 +0300

    Fix undefined index and constant problems

--- readityourself.php
+++ readityourself.php
@@ -26,7 +26,11 @@ require_once dirname(__FILE__).'/inc/rain.tpl.class.php';
 
 
 function url(){
-  $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+  $protocol = "http";
+  if(isset($_SERVER['HTTPS']))
+    if($_SERVER['HTTPS'] != "off")
+      $protocol = "https";
+  
   return $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 }
 
