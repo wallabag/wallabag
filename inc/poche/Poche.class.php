3cc22aab829f339ac6e1ce1af63d646efe4a227c
NumEricR <eric.github@gmail.com>
2013-09-26 23:37:07 +0200

    Avoid ".git" option in themes list of config page

--- inc/poche/Poche.class.php
+++ inc/poche/Poche.class.php
@@ -255,7 +255,7 @@ class Poche
         while (($theme = readdir($handle)) !== false) {
             # Themes are stored in a directory, so all directory names are themes
             # @todo move theme installation data to database
-            if (! is_dir(THEME . '/' . $theme) || in_array($theme, array('..', '.'))) {
+            if (! is_dir(THEME . '/' . $theme) || in_array($theme, array('..', '.', '.git'))) {
                 continue;
             }
             
