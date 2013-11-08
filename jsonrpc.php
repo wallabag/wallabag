2376a8584bb1e7221c879e21e10f12de583a3dfc
Nicolas Lœuillet <nicolas.loeuillet@gmail.com>
2013-11-08 10:27:51 +0100

    [change] fix API call with user_id in parameter

--- jsonrpc.php
+++ jsonrpc.php
@@ -11,93 +11,93 @@ $server->authentication(array(
 ));
 
 // Add a new link
-$server->register('item.add', function ($url) {
+$server->register('item.add', function ($url, $user_id, $fetch_it = true) {
 
-    return Model\add_link($url);
+    return Model\add_link($url, $user_id, $fetch_it);
 });
 
 // Get all bookmark items
-$server->register('item.bookmark.list', function ($offset = null, $limit = null) {
+$server->register('item.bookmark.list', function ($user_id, $offset = null, $limit = null) {
 
-    return Model\get_bookmarks($offset, $limit);
+    return Model\get_bookmarks($user_id, $offset, $limit);
 });
 
 // Count bookmarks
-$server->register('item.bookmark.count', function () {
+$server->register('item.bookmark.count', function ($user_id) {
 
-    return Model\count_bookmarks();
+    return Model\count_bookmarks($user_id);
 });
 
 // Add a bookmark
-$server->register('item.bookmark.create', function ($item_id) {
+$server->register('item.bookmark.create', function ($item_id, $user_id) {
 
-    return Model\set_bookmark_value($item_id, 1);
+    return Model\set_bookmark_value($item_id, 1, $user_id);
 });
 
 // Remove a bookmark
-$server->register('item.bookmark.delete', function ($item_id) {
+$server->register('item.bookmark.delete', function ($item_id, $user_id) {
 
-    return Model\set_bookmark_value($item_id, 0);
+    return Model\set_bookmark_value($item_id, 0, $user_id);
 });
 
 // Get all unread items
-$server->register('item.list_unread', function ($offset = null, $limit = null) {
+$server->register('item.list_unread', function ($user_id, $offset = null, $limit = null) {
 
-    return Model\get_items('unread', $offset, $limit);
+    return Model\get_items('unread', $user_id, $offset, $limit);
 });
 
 // Count all unread items
-$server->register('item.count_unread', function () {
+$server->register('item.count_unread', function ($user_id) {
 
-    return Model\count_entries('unread');
+    return Model\count_entries('unread', $user_id);
 });
 
 // Get all read items
-$server->register('item.list_read', function ($offset = null, $limit = null) {
+$server->register('item.list_read', function ($user_id, $offset = null, $limit = null) {
 
-    return Model\get_items('read', $offset, $limit);
+    return Model\get_items('read', $user_id, $offset, $limit);
 });
 
 // Count all read items
-$server->register('item.count_read', function () {
+$server->register('item.count_read', function ($user_id) {
 
-    return Model\count_entries('read');
+    return Model\count_entries('read', $user_id);
 });
 
 // Get one item
-$server->register('item.info', function ($item_id) {
+$server->register('item.info', function ($item_id, $user_id) {
 
-    return Model\get_item($item_id);
+    return Model\get_item($item_id, $user_id);
 });
 
 // Delete an item
-$server->register('item.delete', function($item_id) {
+$server->register('item.delete', function($item_id, $user_id) {
 
-    return Model\set_item_removed($item_id);
+    return Model\set_item_removed($item_id, $user_id);
 });
 
 // Mark item as read
-$server->register('item.mark_as_read', function($item_id) {
+$server->register('item.mark_as_read', function($item_id, $user_id) {
 
-    return Model\set_item_read($item_id);
+    return Model\set_item_read($item_id, $user_id);
 });
 
 // Mark item as unread
-$server->register('item.mark_as_unread', function($item_id) {
+$server->register('item.mark_as_unread', function($item_id, $user_id) {
 
-    return Model\set_item_unread($item_id);
+    return Model\set_item_unread($item_id, $user_id);
 });
 
 // Change the status of list of items
-$server->register('item.set_list_status', function($status, array $items) {
+$server->register('item.set_list_status', function($status, array $items, $user_id) {
 
-    return Model\set_items_status($status, $items);
+    return Model\set_items_status($status, $items, $user_id);
 });
 
 // Mark all unread items as read
-$server->register('item.mark_all_as_read', function() {
+$server->register('item.mark_all_as_read', function($user_id) {
 
-    return Model\mark_as_read();
+    return Model\mark_as_read($user_id);
 });
 
 // Get one tag
@@ -125,9 +125,9 @@ $server->register('tag.list', function () {
 });
 
 // Get all items with this tag
-$server->register('tag.item.list', function ($tag_id) {
+$server->register('tag.item.list', function ($tag_id, $user_id) {
 
-    return Model\get_entries_by_tag($tag_id);
+    return Model\get_entries_by_tag($tag_id, $user_id);
 });
 
 // Get all tags of an item
--- test_api.php
+++ test_api.php
@@ -7,47 +7,52 @@ use JsonRPC\Client;
 $client = new Client('http://localhost/poche/2/jsonrpc.php');
 $client->authentication('admin', 'PYBRAYc4ebUuRoa');
 
+$user_id = 1;
+
+# poche a link
+// $result = $client->execute('item.add', array('http://cdetc.fr', $user_id));
+
 # fetch all bookmarks
-// $result = $client->execute('item.bookmark.list');
+// $result = $client->execute('item.bookmark.list', array($user_id));
 
 # count bookmarks
-// $result = $client->execute('item.bookmark.count');
+// $result = $client->execute('item.bookmark.count', array($user_id));
 
 # add a bookmark
-// $result = $client->execute('item.bookmark.create', array(1));
+// $result = $client->execute('item.bookmark.create', array(1, $user_id));
 
 # delete a bookmark
-// $result = $client->execute('item.bookmark.delete', array(1));
+// $result = $client->execute('item.bookmark.delete', array(1, $user_id));
 
 # get all unread items
-// $result = $client->execute('item.list_unread');
+// $result = $client->execute('item.list_unread', array($user_id));
 
 # count all unread items
-// $result = $client->execute('item.count_unread');
+// $result = $client->execute('item.count_unread', array($user_id));
 
 # get all read items
-// $result = $client->execute('item.list_read');
+// $result = $client->execute('item.list_read', array($user_id));
 
 # count all read items
-// $result = $client->execute('item.count_read');
+// $result = $client->execute('item.count_read', array($user_id));
 
 # get one item
-// $result = $client->execute('item.info', array(1));
+// $result = $client->execute('item.info', array(1, $user_id));
 
 # delete one item
-// $result = $client->execute('item.delete', array(1));
+// $result = $client->execute('item.delete', array(1, $user_id));
 
 # mark item as read
-// $result = $client->execute('item.mark_as_read', array(1));
+// $result = $client->execute('item.mark_as_read', array(1, $user_id));
 
 # mark item as unread
-// $result = $client->execute('item.mark_as_unread', array(1));
+// $result = $client->execute('item.mark_as_unread', array(1, $user_id));
 
 # change the status of list of items
-// $result = $client->execute('item.set_list_status', array('unread', array(1, 2, 3)));
+// $result = $client->execute('item.set_list_status', array('unread', array(1, 2, 3), $user_id));
 
 # mark all unread items as read
-// $result = $client->execute('item.mark_all_as_read');
+// $result = $client->execute('item.mark_all_as_read', array($user_id));
 
 # get one tag
 // $result = $client->execute('tag.info', array(1));
@@ -62,7 +67,7 @@ $client->authentication('admin', 'PYBRAYc4ebUuRoa');
 // $result = $client->execute('tag.list');
 
 # list of items with this tag
-// $result = $client->execute('tag.item.list', array(2));
+// $result = $client->execute('tag.item.list', array(2, $user_id));
 
 # list of tags for an item
 // $result = $client->execute('item.tag.list', array(3));
