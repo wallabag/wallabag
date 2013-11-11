7d7446cfb1ebcd37b827a7941b80037fb37a685a
Matthias Baur <thats@matthiasbaur.me>
2013-11-11 22:11:42 +0100

    Added possibility to mark items as read/unread on the item page itself.

--- templates/show_item.php
+++ templates/show_item.php
@@ -37,6 +37,25 @@
                     data-item-id="<?php echo $item['id'] ?>"
                 >☆</a>
             <?php endif ?> |
+            <?php if ($item['status'] == 'unread'): ?>
+                <a
+                    id="status-<?php echo $item['id'] ?>"
+                    href="?action=mark-item-read&amp;id=<?php echo $item['id'] ?>&amp;offset=<?php echo $offset ?>&amp;redirect=<?php echo $redirect ?>"
+                    data-action="mark-read"
+                    data-item-id="<?php echo $item['id'] ?>"
+                    data-reverse-label="<?php echo t('mark as unread') ?>"
+                >
+                    <?php echo t('mark as read') ?></a> |
+            <?php else: ?>
+                <a
+                    id="status-<?php echo $item['id'] ?>"
+                    href="?action=mark-item-unread&amp;id=<?php echo $item['id'] ?>&amp;offset=<?php echo $offset ?>&amp;redirect=<?php echo $redirect ?>"
+                    data-action="mark-unread"
+                    data-item-id="<?php echo $item['id'] ?>"
+                    data-reverse-label="<?php echo t('mark as read') ?>"
+                >
+                    <?php echo t('mark as unread') ?></a> |
+            <?php endif ?>
 
             <span class="hide-mobile"><?php echo dt('%e %B %Y %k:%M', $item['updated']) ?> |</span>
 
