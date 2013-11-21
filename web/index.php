f0a552012ea4cc191d9c22d6117e778ac9bcefdc
Vincent Jousse <vincent@jousse.org>
2013-11-21 09:54:33 +0100

    Add front controller

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+
+$app->run();
