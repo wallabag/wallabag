9e6c26a2039a8fe31b5df4d1fd7f566d6de0aff8
Vincent Jousse <vincent@jousse.org>
2013-11-21 10:15:29 +0100

    Bootstrap files and directories

--- web/index.php
+++ web/index.php
@@ -9,5 +9,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+require_once __DIR__.'/../src/app.php';
+require_once __DIR__.'/../src/controllers.php';
+
+$app['debug'] = true;
 
 $app->run();
