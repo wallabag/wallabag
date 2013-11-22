e71f24ec639a078f5a0c39455c1ea7b09ac613b1
Vincent Jousse <vincent@jousse.org>
2013-11-22 15:03:25 +0100

    Move controllers in a dedicated directory

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ if (php_sapi_name() === 'cli-server' && is_file($filename)) {
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../app/app.php';
-require_once __DIR__.'/../app/controllers.php';
+require_once __DIR__.'/../app/controllers/controllers.php';
 
 $app['debug'] = true;
 
