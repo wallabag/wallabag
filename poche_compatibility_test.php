fcb5fd27e2fe7de36a9e2dffc818686a08ecc4db
Nicolas Lœuillet <nicolas.loeuillet@gmail.com>
2014-01-06 08:32:28 +0100

    [change] update poche version in compatibility test file

--- poche_compatibility_test.php
+++ poche_compatibility_test.php
@@ -1,5 +1,5 @@
 <?php
-$app_name = 'poche 1.0';
+$app_name = 'poche 1.3';
 
 $php_ok = (function_exists('version_compare') && version_compare(phpversion(), '5.3.3', '>='));
 $pcre_ok = extension_loaded('pcre');
