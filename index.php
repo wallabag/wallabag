7256e9e139eb07cdae1fbe3009454e6c7d9a8677
Nicolas Lœuillet <nicolas@loeuillet.org>
2014-04-11 17:08:11 +0200

    1.6.1 version

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * @license    http://www.wtfpl.net/ see COPYING file
  */
 
-define ('POCHE', '1.6.0');
+define ('POCHE', '1.6.1');
 require 'check_setup.php';
 require_once 'inc/poche/global.inc.php';
 
