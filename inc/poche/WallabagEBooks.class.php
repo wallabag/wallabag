f8c37985224313533111e5b1d78b5a20eeff8b04
tcit <tcit@tcit.fr>
2014-07-26 11:52:43 +0200

    little better

--- inc/poche/WallabagEBooks.class.php
+++ inc/poche/WallabagEBooks.class.php
@@ -16,6 +16,7 @@ class WallabagEBooks
     protected $entries;
     protected $bookTitle;
     protected $bookFileName;
+    protected $author = 'wallabag';
 
     public function __construct(Poche $wallabag, $method, $value)
     {
@@ -33,11 +34,14 @@ class WallabagEBooks
                 $this->entries = array($entry);
                 $this->bookTitle = $entry['title'];
                 $this->bookFileName = substr($this->bookTitle, 0, 200);
+                $this->author = preg_replace('#^w{3}.#', '', Tools::getdomain($entry["url"])); # if only one article, set author to domain name (we strip the eventual www part)
+                Tools::logm('Producing ebook from article ' . $this->bookTitle);
                 break;
             case 'all':
                 $this->entries = $this->wallabag->store->retrieveAll($this->wallabag->user->getId());
                 $this->bookTitle = sprintf(_('All my articles on %s'), date(_('d.m.y'))); #translatable because each country has it's own date format system
                 $this->bookFileName = _('Allarticles') . date(_('dmY'));
+                Tools::logm('Producing ebook from all articles');
                 break;
             case 'tag':
                 $tag = filter_var($this->value, FILTER_SANITIZE_STRING);
@@ -46,12 +50,14 @@ class WallabagEBooks
                 $this->entries = $this->wallabag->store->retrieveEntriesByTag($tag_id, $this->wallabag->user->getId());
                 $this->bookTitle = sprintf(_('Articles tagged %s'), $tag);
                 $this->bookFileName = substr(sprintf(_('Tag %s'), $tag), 0, 200);
+                Tools::logm('Producing ebook from tag ' . $tag);
                 break;
             case 'category':
                 $category = filter_var($this->value, FILTER_SANITIZE_STRING);
                 $this->entries = $this->wallabag->store->getEntriesByView($category, $this->wallabag->user->getId());
                 $this->bookTitle = sprintf(_('All articles in category %s'), $category);
                 $this->bookFileName = substr(sprintf(_('Category %s'), $category), 0, 200);
+                Tools::logm('Producing ebook from category ' . $category);
                 break;
             case 'search':
                 $search = filter_var($this->value, FILTER_SANITIZE_STRING);
@@ -59,9 +65,10 @@ class WallabagEBooks
                 $this->entries = $this->wallabag->store->search($search, $this->wallabag->user->getId());
                 $this->bookTitle = sprintf(_('All articles for search %s'), $search);
                 $this->bookFileName = substr(sprintf(_('Search %s'), $search), 0, 200);
+                Tools::logm('Producing ebook from search ' . $search);
                 break;
             case 'default':
-                die(_('Uh, there is a problem while generating epub.'));
+                die(_('Uh, there is a problem while generating eBook.'));
         }
     }
 }
@@ -73,12 +80,13 @@ class WallabagEpub extends WallabagEBooks
      */
     public function produceEpub()
     {
+        Tools::logm('Starting to produce ePub 3 file');
         $content_start =
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
             . "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\">\n"
             . "<head>"
             . "<meta http-equiv=\"Default-Style\" content=\"text/html; charset=utf-8\" />\n"
-            . "<title>wallabag articles book</title>\n"
+            . "<title>" . _("wallabag articles book") . "</title>\n"
             . "</head>\n"
             . "<body>\n";
 
@@ -95,11 +103,13 @@ class WallabagEpub extends WallabagEBooks
         $log->logLine("getCurrentServerURL: " . $book->getCurrentServerURL());
         $log->logLine("getCurrentPageURL..: " . $book->getCurrentPageURL());
 
+        Tools::logm('Filling metadata for ePub...');
+
         $book->setTitle($this->bookTitle);
         $book->setIdentifier("http://$_SERVER[HTTP_HOST]", EPub::IDENTIFIER_URI); // Could also be the ISBN number, prefered for published books, or a UUID.
         //$book->setLanguage("en"); // Not needed, but included for the example, Language is mandatory, but EPub defaults to "en". Use RFC3066 Language codes, such as "en", "da", "fr" etc.
         $book->setDescription(_("Some articles saved on my wallabag"));
-        $book->setAuthor("wallabag", "wallabag");
+        $book->setAuthor($this->author,$this->author);
         $book->setPublisher("wallabag", "wallabag"); // I hope this is a non existant address :)
         $book->setDate(time()); // Strictly not needed as the book date defaults to time().
         //$book->setRights("Copyright and licence information specific for the book."); // As this is generated, this _could_ contain the name or licence information of the user who purchased the book, if needed. If this is used that way, the identifier must also be made unique for the book.
@@ -123,6 +133,8 @@ class WallabagEpub extends WallabagEBooks
 
         $book->buildTOC();
 
+        Tools::logm('Adding actual content...');
+
         foreach ($this->entries as $entry) { //set tags as subjects
             $tags = $this->wallabag->store->retrieveTagsByEntry($entry['id']);
             foreach ($tags as $tag) {
@@ -138,9 +150,11 @@ class WallabagEpub extends WallabagEBooks
 
         if (DEBUG_POCHE) {
             $book->addChapter("Log", "Log.html", $content_start . $log->getLog() . "\n</pre>" . $bookEnd); // log generation
+            Tools::logm('Production log available in produced file');
         }
         $book->finalize();
         $zipData = $book->sendBook($this->bookFileName);
+        Tools::logm('Ebook produced');
     }
 } 
 
@@ -151,24 +165,27 @@ class WallabagMobi extends WallabagEBooks
 	* @author Sander Kromwijk
 	*/
 
-    private $_kindle_email;
-
 	public function produceMobi($sendByMail = FALSE)
 	{
 
+        Tools::logm('Starting to produce Mobi file');
         $mobi = new MOBI();
         $content = new MOBIFile();
 
         $messages = new Messages(); // for later
-            
+        
+        Tools::logm('Filling metadata for Mobi...');
+
         $content->set("title", $this->bookTitle);
-        $content->set("author", "wallabag");
+        $content->set("author", $this->author);
         $content->set("subject", $this->bookTitle);
 
         # introduction
         $content->appendParagraph('<div style="text-align:center;" ><p>' . _('Produced by wallabag with PHPMobi') . '</p><p>'. _('Please open <a href="https://github.com/wallabag/wallabag/issues" >an issue</a> if you have trouble with the display of this E-Book on your device.') . '</p></div>');
         $content->appendImage(imagecreatefrompng("themes/baggy/img/apple-touch-icon-152.png"));
         $content->appendPageBreak();
+
+        Tools::logm('Adding actual content...');
 
         foreach ($this->entries as $item) {
             $content->appendChapterTitle($item['title']);
@@ -180,9 +197,18 @@ class WallabagMobi extends WallabagEBooks
         if (!$sendByMail) {
             // we offer file to download
             $mobi->download($this->bookFileName.'.mobi');
+            Tools::logm('Mobi file produced');
         }
         else {
             // we send file to kindle
+
+            Tools::logm('Preparing to send file by email');
+
+            $error = FALSE;
+            # testing Mail function
+            if (!function_exists('mail')) {
+                $error = _('Mail function is unavailable');
+            }
 
             $char_in = array('/', '.', ',', ':', '|'); # we sanitize filename to avoid conflicts with special characters (for instance, / goes for a directory)
             $mobiExportName = preg_replace('/\s+/', '-', str_replace($char_in, '-', $this->bookFileName)) . '.mobi';
@@ -216,15 +242,17 @@ class WallabagMobi extends WallabagEBooks
             $header .= "--".$uid."--";
 
             # trying to get the kindle email adress
-            if ($this->wallabag->user->getConfigValue('kindleemail')) 
+            if (!$this->wallabag->user->getConfigValue('kindleemail')) 
             {
-                #do a try...exeption here
-                mail( $this->wallabag->user->getConfigValue('kindleemail'), '[wallabag] ' . $this->bookTitle, "", $header );
-                $messages->add('s', _('The email has been sent to your kindle !'));
+                $error = _('You didn\'t set your kindle\'s email adress !');
             }
-            else
-            {
-                $messages->add('e', _('You didn\'t set your kindle\'s email adress !'));
+            if (!$error) {
+                mail($this->wallabag->user->getConfigValue('kindleemail'), '[wallabag] ' . $this->bookTitle, "", $header );
+                $messages->add('s', _('The email has been sent to your kindle !'));
+                Tools::logm('Email sent');
+            } else {
+                $messages->add('e', $error);
+                Tools::logm($error);
             }
         }
     }
@@ -236,8 +264,15 @@ class WallabagPDF extends WallabagEbooks
 	{
 		$mpdf = new mPDF('c'); 
 
-        # intro
+        Tools::logm('Starting to produce PDF file');
 
+        Tools::logm('Filling metadata for PDF...');
+
+        # headers
+        $mpdf->SetHeader('{DATE j-m-Y}|{PAGENO}/{nb}|Produced with wallabag');
+        $mpdf->SetFooter('{PAGENO}');
+
+        # intro
         $html = '<h1>' . $this->bookTitle . '<bookmark content="Cover" /></h1><div style="text-align:center;" >
         <p>' . _('Produced by wallabag with mPDF') . '</p>
         <p>'. _('Please open <a href="https://github.com/wallabag/wallabag/issues" >an issue</a> if you have trouble with the display of this E-Book on your device.') . '</p>
@@ -245,6 +280,7 @@ class WallabagPDF extends WallabagEbooks
         $html .= '<pagebreak type="next-odd" />';
         $i = 1;
 
+        Tools::logm('Adding actual content...');
         foreach ($this->entries as $item) {
             $html .= '<h1>' . $item['title'] . '<bookmark content="' . $item['title'] . '" /></h1>';
             $html .= '<indexentry content="'. $item['title'] .'" />';
@@ -252,17 +288,12 @@ class WallabagPDF extends WallabagEbooks
             $html .= '<pagebreak type="next-odd" />';
             $i = $i+1;
         }
-
-
-        # headers
-        $mpdf->SetHeader('{DATE j-m-Y}|{PAGENO}/{nb}|Produced with wallabag');
-        $mpdf->SetFooter('{PAGENO}');
 		
         $mpdf->WriteHTML($html);
 
         # remove characters that make mpdf bug
         $char_in = array('/', '.', ',', ':', '|');
-        $pdfExportName = preg_replace('/\s+/', '-', str_replace($char_in, '-', $this->bookFileName . '.pdf'));
+        $pdfExportName = preg_replace('/\s+/', '-', str_replace($char_in, '-', $this->bookFileName . '.pdf')); # maybe someone can make a proper regex of this ?
 
         # index
         $html = '<h2>Index<bookmark content="Index" /></h2>
@@ -278,7 +309,6 @@ class WallabagPDF extends WallabagEbooks
         header('Content-Transfer-Encoding: base64');
         header('Content-Type: application/pdf');
         echo file_get_contents('cache/' . $pdfExportName);
-
-        //exit;
+        Tools::logm('PDF file produced');
 	}
 }
