2d4cfc58ec324987ad39365cbb3e4eb49df4e426
Mariusz Kozakowski <11mariom+wordpress@gmail.com>
2014-09-17 18:44:29 +0200

    Add support for custom http port
    Now you can use wallabag behind reverse proxy (i.e Squid or Varnish)
    without problem with urls like wallabag.example.com:8080.

--- inc/poche/Tools.class.php
+++ inc/poche/Tools.class.php
@@ -51,6 +51,7 @@ final class Tools
 
         $serverport = (!isset($_SERVER["SERVER_PORT"])
             || $_SERVER["SERVER_PORT"] == '80'
+            || $_SERVER["SERVER_PORT"] == HTTP_PORT
             || ($https && $_SERVER["SERVER_PORT"] == '443')
             || ($https && $_SERVER["SERVER_PORT"]==SSL_PORT) //Custom HTTPS port detection
             ? '' : ':' . $_SERVER["SERVER_PORT"]);
