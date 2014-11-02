ff02fd8aca56a63e8caf3b9f87de8a677dd419e9
Jay Sitter <jsitter@gmail.com>
2014-11-02 11:15:36 -0500

    Improving JavaScript for popup forms

--- themes/default/js/popupForm.js
+++ themes/default/js/popupForm.js
@@ -1,29 +1,72 @@
 $(document).ready(function() {
 
     $("#search-form").hide();
+    $("#bagit-form").hide();
 
-    function closeSearch() {
+    //---------------------------------------------------------------------------
+    // Toggle the "Search" popup in the sidebar
+    //---------------------------------------------------------------------------
+    function toggleSearch() {
         $("#search-form").toggle();
         $("#search").toggleClass("current");
+        $("#search").toggleClass("active-current");
         $("#search-arrow").toggleClass("arrow-down");
+        if ($("#search").hasClass("current")) {
+            $("#content").addClass("opacity03");
+        } else {
+            $("#content").removeClass("opacity03");
+        }
+    }
+
+    //---------------------------------------------------------------------------
+    // Toggle the "Save a Link" popup in the sidebar
+    //---------------------------------------------------------------------------
+    function toggleBagit() {
+        $("#bagit-form").toggle();
+        $("#bagit").toggleClass("current");
+        $("#bagit").toggleClass("active-current");
+        $("#bagit-arrow").toggleClass("arrow-down");
+        if ($("#bagit").hasClass("current")) {
+            $("#content").addClass("opacity03");
+        } else {
+            $("#content").removeClass("opacity03");
+        }
+    }
+
+    //---------------------------------------------------------------------------
+    // Close all #links popups in the sidebar
+    //---------------------------------------------------------------------------
+    function closePopups() {
+        $("#links .messages").hide();
+        $("#links > li > a").removeClass("active-current");
+        $("#links > li > a").removeClass("current");
+        $("[id$=-arrow]").removeClass("arrow-down");
+        $("#content").removeClass("opacity03");
     }
 
     $("#search").click(function(){
-        closeSearch();
-        // if other popup is already shown
-        if ($("#bagit-form").length != 0) {
-            $("#bagit").removeClass("active-current");
-            $('#content').removeClass("opacity03");
-            $("#bagit").removeClass("current");
-            $("#bagit-arrow").removeClass("arrow-down");
-            $("#bagit-form").hide();
-        }
-        $('#searchfield').focus();
+        closePopups();
+        toggleSearch();
+        $("#searchfield").focus();
+    });
+
+    $("#bagit").click(function(){
+        closePopups();
+        toggleBagit();
+        $("#plainurl").focus();
     });
 
     $("#search-form-close").click(function(){
-        closeSearch();
+        toggleSearch();
     });
+
+    $("#bagit-form-close").click(function(){
+        toggleBagit();
+    });
+
+    // $("#").click(function(){
+    //     toggleSearch();
+    // });
 
 
 });
