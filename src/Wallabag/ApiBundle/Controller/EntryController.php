38ba7ed9721ef2fefc0cf76d37bfbb93eb6b5c76
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-01-28 18:18:57 +0100

    skeleton for DELETE and PATCH methods

--- src/Wallabag/ApiBundle/Controller/EntryController.php
+++ src/Wallabag/ApiBundle/Controller/EntryController.php
@@ -6,17 +6,19 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Wallabag\CoreBundle\Entity\Entries;
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use FOS\RestBundle\Controller\Annotations\Patch;
 use Wallabag\CoreBundle\Entity\Users;
 
 class EntryController extends Controller
 {
     /**
-     * Fetch an entry for a given user
+     * Fetches an entry for a given user
      *
      * @Get("/u/{user}/entry/{entry}")
      * @ApiDoc(
      *      requirements={
-     *          {"name"="user", "dataType"="string", "requirement"="\w+", "description"="The username"},
+     *          {"name"="user", "dataType"="string", "requirement"="\w+", "description"="The user ID"},
      *          {"name"="entry", "dataType"="integer", "requirement"="\w+", "description"="The entry ID"}
      *      }
      * )
@@ -25,5 +27,37 @@ class EntryController extends Controller
     public function getAction(Users $user, Entries $entry)
     {
         return $entry;
+    }
+
+    /**
+     * Deletes an entry for a given user
+     *
+     * @Delete("/u/{user}/entry/{entry}")
+     * @ApiDoc(
+     *      requirements={
+     *          {"name"="user", "dataType"="string", "requirement"="\w+", "description"="The user ID"},
+     *          {"name"="entry", "dataType"="integer", "requirement"="\w+", "description"="The entry ID"}
+     *      }
+     * )
+     */
+    public function deleteAction(Users $user, Entries $entry)
+    {
+
+    }
+
+    /**
+     * Changes several properties of an entry. I.E tags, archived, starred and deleted status
+     *
+     * @Patch("/u/{user}/entry/{entry}")
+     * @ApiDoc(
+     *      requirements={
+     *          {"name"="user", "dataType"="string", "requirement"="\w+", "description"="The user ID"},
+     *          {"name"="entry", "dataType"="integer", "requirement"="\w+", "description"="The entry ID"}
+     *      }
+     * )
+     */
+    public function patchAction(Users $user, Entries $entry)
+    {
+
     }
 }
