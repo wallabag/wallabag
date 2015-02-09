7ffb1e80bff2c54fe3fe4343484d377eda660c3f
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-02-09 22:54:29 +0100

    replace services.xml into services.yml

--- src/Wallabag/CoreBundle/DependencyInjection/WallabagCoreExtension.php
+++ src/Wallabag/CoreBundle/DependencyInjection/WallabagCoreExtension.php
@@ -3,7 +3,7 @@
 namespace Wallabag\CoreBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
@@ -11,8 +11,8 @@ class WallabagCoreExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 
     public function getAlias()
