f5deb024a2dd5a2cb27263a51f4653609407597e
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-02-10 20:07:55 +0100

    add test for api/salts

--- src/Wallabag/CoreBundle/Tests/Controller/WallabagRestControllerTest.php
+++ src/Wallabag/CoreBundle/Tests/Controller/WallabagRestControllerTest.php
@@ -6,7 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class WallabagRestControllerTest extends WebTestCase
 {
-    public function testEmptyGetEntries() {
+    public function testGetSalt()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/api/salts/admin.json');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $client->request('GET', '/api/salts/notfound.json');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testEmptyGetEntries()
+    {
         $client = $this->createClient();
         $client->request('GET', '/api/entries');
         $this->assertTrue($client->getResponse()->isOk());
