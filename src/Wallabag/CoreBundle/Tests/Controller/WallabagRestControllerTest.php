19aee7cd545a904d830037e8df785b1703ac62fa
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-02-11 06:41:44 +0100

    refactor and test one entry

--- src/Wallabag/CoreBundle/Tests/Controller/WallabagRestControllerTest.php
+++ src/Wallabag/CoreBundle/Tests/Controller/WallabagRestControllerTest.php
@@ -6,6 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class WallabagRestControllerTest extends WebTestCase
 {
+    /**
+     * Generate HTTP headers for authenticate user on API
+     *
+     * @param $username
+     * @param $password
+     * @param $salt
+     *
+     * @return array
+     */
+    private function generateHeaders($username, $password, $salt)
+    {
+        $encryptedPassword = sha1($password.$username.$salt);
+        $nonce = substr(md5(uniqid('nonce_', true)), 0, 16);
+
+        $now = new \DateTime('now', new \DateTimeZone('UTC'));
+        $created = (string) $now->format('Y-m-d\TH:i:s\Z');
+        $digest = base64_encode(sha1(base64_decode($nonce).$created.$encryptedPassword, true));
+
+        return array(
+            'PHP_AUTH_USER' => 'username',
+            'HTTP_AUTHORIZATION' => 'Authorization profile="UsernameToken"',
+            'HTTP_x-wsse' => 'X-WSSE: UsernameToken Username="'.$username.'", PasswordDigest="'.$digest.'", Nonce="'.$nonce.'", Created="'.$created.'"',
+        );
+    }
+
     public function testGetSalt()
     {
         $client = $this->createClient();
@@ -16,28 +41,32 @@ class WallabagRestControllerTest extends WebTestCase
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
     }
 
+    public function testGetOneEntry()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/api/salts/admin.json');
+        $content = json_decode($client->getResponse()->getContent());
+
+        $headers = $this->generateHeaders('admin', 'test', $content[0]);
+
+        $client->request('GET', '/api/entries/1.json', array(), array(), $headers);
+        $this->assertContains('This is my content', $client->getResponse()->getContent());
+
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            )
+        );
+    }
+
     public function testGetEntries()
     {
         $client = $this->createClient();
         $client->request('GET', '/api/salts/admin.json');
         $content = json_decode($client->getResponse()->getContent());
-        $salt = $content[0];
 
-        $username = 'admin';
-        $password = 'test';
-
-        $encryptedPassword = sha1($password.$username.$salt);
-        $nonce = substr(md5(uniqid('nonce_', true)), 0, 16);
-
-        $now = new \DateTime('now', new \DateTimeZone('UTC'));
-        $created = (string) $now->format('Y-m-d\TH:i:s\Z');
-        $digest = base64_encode(sha1(base64_decode($nonce).$created.$encryptedPassword, true));
-
-        $headers = array(
-            'PHP_AUTH_USER' => 'username',
-            'HTTP_AUTHORIZATION' => 'Authorization profile="UsernameToken"',
-            'HTTP_x-wsse' => 'X-WSSE: UsernameToken Username="'.$username.'", PasswordDigest="'.$digest.'", Nonce="'.$nonce.'", Created="'.$created.'"',
-        );
+        $headers = $this->generateHeaders('admin', 'test', $content[0]);
 
         $client->request('GET', '/api/entries', array(), array(), $headers);
         $this->assertContains('Mailjet', $client->getResponse()->getContent());
