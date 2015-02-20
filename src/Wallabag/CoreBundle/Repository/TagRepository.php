b9dd02b2ef0210ae3602239718a9f599ab1c8ee6
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-02-20 20:29:33 +0100

    add relation between entry and tag

--- src/Wallabag/CoreBundle/Repository/TagRepository.php
+++ src/Wallabag/CoreBundle/Repository/TagRepository.php
@@ -6,5 +6,18 @@ use Doctrine\ORM\EntityRepository;
 
 class TagRepository extends EntityRepository
 {
+    public function findByEntries($entryId)
+    {
+        $qb = $this->createQueryBuilder('t')
+            ->select('t')
+            ->leftJoin('t.id', 'u')
+            ->where('e.isStarred = true')
+            ->andWhere('u.id =:userId')->setParameter('userId', $userId)
+            ->orderBy('e.createdAt', 'desc')
+            ->getQuery();
 
+        $paginator = new Paginator($qb);
+
+        return $paginator;
+    }
 }
