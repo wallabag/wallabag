dcae2fc25d81229d15a8ace1949d6f443ab6036f
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-02-27 21:26:43 +0100

    enable CORS

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
             new Wallabag\CoreBundle\WallabagCoreBundle()
         );
 
