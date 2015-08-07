768303a573e20b80911c6442f22fa3cda3caa550
Alexandr Danilov <bitbucket@modos189.ru>
2015-08-07 23:29:21 +0300

    fix #1282

--- src/Wallabag/CoreBundle/Resources/views/themes/material/public/js/init.js
+++ src/Wallabag/CoreBundle/Resources/views/themes/material/public/js/init.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
         $(".nav-panel-buttom").hide(100);
         $(".nav-panel-search").show(100);
         $(".nav-panel-menu").addClass('hidden');
+        $(".nav-panels").css('background', 'white');
         return false;
     });
     $('.mdi-navigation-close').click(function(){
@@ -23,6 +24,7 @@ $(document).ready(function(){
         $(".nav-panel-search").hide(100);
         $(".nav-panel-buttom").show(100);
         $(".nav-panel-menu").removeClass('hidden');
+        $(".nav-panels").css('background', 'transparent');
         return false;
     });
 });
