3c5b025ac369c99df9ed0c83fefaac436e13b33c
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-08-14 22:57:16 +0200

    filter by creation date

--- src/Wallabag/CoreBundle/Filter/EntryFilterType.php
+++ src/Wallabag/CoreBundle/Filter/EntryFilterType.php
@@ -10,7 +10,21 @@ class EntryFilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('readingTime', 'filter_number_range');
+        $builder
+            ->add('readingTime', 'filter_number_range')
+            ->add('createdAt', 'filter_date_range', array(
+                    'left_date_options' => array(
+                        'attr' => array(
+                            'placeholder' => 'dd/mm/yyyy'),
+                        'format' => 'dd/MM/yyyy',
+                        'widget' => 'single_text'
+                    ),
+                    'right_date_options' => array(
+                        'attr' => array(
+                            'placeholder' => 'dd/mm/yyyy'),
+                        'format' => 'dd/MM/yyyy',
+                        'widget' => 'single_text'
+                    )));
     }
 
     public function getName()
