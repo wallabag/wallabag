c79c16a96761790ebfd126a207efca17d28fdf01
Nicolas Lœuillet <nicolas@loeuillet.org>
2015-09-13 08:24:57 +0200

    FOSOAuthServerBundle installation & configuration

---
+++ src/Wallabag/ApiBundle/Entity/AccessToken.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wallabag\ApiBundle\Entity;
+
+use FOS\OAuthServerBundle\Entity\AccessToken as BaseAccessToken;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table("oauth2_access_tokens")
+ * @ORM\Entity
+ */
+class AccessToken extends BaseAccessToken
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Client")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $client;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Wallabag\CoreBundle\Entity\User")
+     */
+    protected $user;
+}
