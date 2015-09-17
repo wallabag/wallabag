a237363f073a899a99f287848374e24b64e0ac66
Thomas Citharel <tcit@tcit.fr>
2015-09-17 22:42:41 +0200

    improve comments system & display & deleting comments

--- src/Wallabag/CoreBundle/Controller/CommentController.php
+++ src/Wallabag/CoreBundle/Controller/CommentController.php
@@ -14,7 +14,7 @@ class CommentController extends Controller
     /**
      * @param Request $request
      *
-     * @Route("/new-comment/{entry}", requirements={"entry" = "\d+"}, name="new_comment")
+     * @Route("/comment/new/{entry}", requirements={"entry" = "\d+"}, name="new_comment")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
@@ -28,7 +28,6 @@ class CommentController extends Controller
             $em = $this->getDoctrine()->getManager();
 
             $entry->addComment($comment);
-            $comment->setEntry($entry);
 
             $em->persist($comment);
 
@@ -47,4 +46,47 @@ class CommentController extends Controller
             'entry' => $entry,
         ));
     }
+
+    /**
+     * Deletes comment
+     *
+     * @param Comment $comment
+     * @param Request $request
+     *
+     * @Route("/comment/delete/{id}", requirements={"id" = "\d+"}, name="delete_comment")
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteCommentAction(Request $request, Comment $comment)
+    {
+        $this->checkUserAction($comment);
+
+        if (!$comment) {
+            throw $this->createNotFoundException('No comment found');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($comment);
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            'Comment deleted'
+        );
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+    /**
+     * Check if the logged user can manage the given comment.
+     *
+     * @param Comment $comment
+     */
+    private function checkUserAction(Comment $comment)
+    {
+        if ($this->getUser()->getId() != $comment->getUser()->getId()) {
+            throw $this->createAccessDeniedException('You can not access this entry.');
+        }
+    }
+
 }
---
+++ src/Wallabag/CoreBundle/Repository/CommentRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wallabag\CoreBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class CommentRepository extends EntityRepository
+{
+
+}
