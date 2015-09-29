db22a4928fb9740915f2cb2a7b23ad5bae528869
Thomas Citharel <tcit@tcit.fr>
2015-09-29 22:46:13 +0200

    clean code

--- src/Wallabag/CoreBundle/Resources/views/themes/material/public/js/init.js
+++ src/Wallabag/CoreBundle/Resources/views/themes/material/public/js/init.js
@@ -12,92 +12,89 @@ function init_filters() {
 }
 
 function init_comments() {
-  var offsetcom, domPath, paragraph;
-  var paragraphsSelector = $('p, blockquote > p, aside > p');
-  var commentselector = $('.comment');
-  var comment = new Array();
-  var position = new Array();
-  var commentdomints = $('.commentdom');
+    var offsetcom, domPath;
+    var commentselector = $('.comment');
+    var comment = new Array();
+    var position = new Array();
+    var commentdomints = $('.commentdom');
 
-  for (var i = 0; i < commentselector.length; i++) {
+    for (var i = 0; i < commentselector.length; i++) {
 
-    // define paragraph for comment
-    comment['dom'] = commentdomints.eq(i).text();
-    //console.log(comment['dom']);
+        // define paragraph for comment
+        comment['dom'] = commentdomints.eq(i).text();
+        //console.log(comment['dom']);
 
-    // get paragraph associated to comment
-    comment['paragraph'] = $("p[data-wallabag-paragraph='" + comment['dom'] + "']");
+        // get paragraph associated to comment
+        comment['paragraph'] = $("p[data-wallabag-paragraph='" + comment['dom'] + "']");
+
+        /*
+         * If the screen is big enough and if comments have a dom position, show comments aside
+         */
+        if (window.matchMedia("(min-width:992px)").matches && comment['dom'] != 'false') {
+            comment['paragraph'].after(commentselector.eq(i));
+            position[i] = comment['paragraph'].position();
+            commentselector.eq(i).css('position','absolute');
+            commentselector.eq(i).css('width','250px');
+            commentselector.eq(i).css('top', position[i].top.toString() + 'px'); // take them next to the paragraph
+            commentselector.eq(i).css('left', em(88).toString() + 'px');
+            if ($('#comments_list').children().length < 2) { // if there isn't any comments to show on bottom, hide list of comments
+                $('#comments_list').hide();
+            }
+        } else
+        /*
+         * If the screen is small, show the comments on bottom of the article, with an anchor to get up
+         */
+        {
+            commentselector.eq(i).css('max-width','40em');
+            // set anchor for return button to paragraph
+            if (comment['dom'] != 'false') {
+                $('.return-to-paragraph').eq(i).css('display','inline');
+                commentselector.eq(i).find('.return-to-paragraph').attr('href', '#' + comment['paragraph'].attr('id'));
+            }
+        }
+    };
+
+
 
     /*
-     * If the screen is big enough, show comments
+     * Hide comment form if user clicks elsewhere
      */
-    if (window.matchMedia("(min-width:992px)").matches && comment['dom'] != 'false') {
-      comment['paragraph'].after(commentselector.eq(i));
-      position[i] = comment['paragraph'].position();
-      commentselector.eq(i).css('position','absolute');
-      commentselector.eq(i).css('width','250px');
-      commentselector.eq(i).css('top', position[i].top.toString() + 'px');
-      commentselector.eq(i).css('left', em(88).toString() + 'px');
-      if ($('#comments_list').children().length < 2) {
-        $('#comments_list').hide();
-      }
 
-    } else
+    $(document).mouseup(function (e)
+    {
+        var container = $(".nav-panel-add-comment");
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            container.hide();
+        }
+    });
+
     /*
-     * If the screen is small, show the comments on bottom of the article, with an anchor to get up
+     * Show comment button when hoovering a paragraph
      */
-    {
-        commentselector.eq(i).css('max-width','40em');
-        // set anchor for return button to paragraph
-        if (comment['dom'] != 'false') {
-            $('.return-to-paragraph').eq(i).css('display','inline');
-            commentselector.eq(i).find('.return-to-paragraph').attr('href', '#' + comment['paragraph'].attr('id'));
-      }
-    }
-  };
-
-
-
-  /*
-   * Hide comment form if user clicks elsewhere
-   */
-
-  $(document).mouseup(function (e)
-  {
-    var container = $(".nav-panel-add-comment");
-
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        container.hide();
-    }
-  });
-
-  /*
-   * Show comment button when hoovering a paragraph
-   */
 
     if (window.matchMedia("(min-width:992px)").matches) {
         $('article p, article blockquote, article aside').mouseover(function(e) {
-            paragraph = $(this);
-            offsetcom = paragraph.offset();
+            offsetcom = $(this).offset();
             domPath = $(this).attr('data-wallabag-paragraph');
             $("#new-comment-button").show();
             $("#new-comment-button").offset({ top: offsetcom.top, left: (offsetcom.left + em(55)) });
             $("#new-comment-button").css('display','inline-block');
         });
     }
+
   /*
    * Add the comment input textarea when clicking on button
    */
 
-  $('#new-comment-button').click(function(e) {
-    $('.nav-panel-add-comment').show();
-    $(".nav-panel-add-comment").offset({ top: offsetcom.top, left: offsetcom.left + em(57)});
-    $("#comment_content").focus();
-    $(".nav-panel-add-comment #comment_dom").val(domPath);
-});
-
+    $('#new-comment-button').click(function(e) {
+        $('.nav-panel-add-comment').show();
+        $(".nav-panel-add-comment").offset({ top: offsetcom.top, left: offsetcom.left + em(57)});
+        $("#comment_content").focus();
+        $(".nav-panel-add-comment #comment_dom").val(domPath);
+    });
 }
 
 /*
