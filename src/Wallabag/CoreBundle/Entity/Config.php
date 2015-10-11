1d7b350b259ccc0110318a377ae3b3752ec6b9fb
Kévin Gomez <contact@kevingomez.fr>
2015-10-11 22:22:24 +0200

    Add missing use statement

--- src/Wallabag/CoreBundle/Entity/Config.php
+++ src/Wallabag/CoreBundle/Entity/Config.php
@@ -2,6 +2,7 @@
 
 namespace Wallabag\CoreBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
