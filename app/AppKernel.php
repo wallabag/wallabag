1b8b79da8c20693e11af3a35aaf3389eebe8669e
Nicolas Lœuillet <nicolas.loeuillet@smile.fr>
2015-10-14 21:30:25 +0200

    add slug for tags

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new Wallabag\UserBundle\WallabagUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
