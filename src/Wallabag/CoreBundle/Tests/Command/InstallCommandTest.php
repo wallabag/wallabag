970e0e994f62d59e25ba42de4502714ab0ab08a1
Jeremy Benoist <jeremy.benoist@gmail.com>
2015-11-07 14:18:38 +0100

    Remove duplicate comments

--- src/Wallabag/CoreBundle/Tests/Command/InstallCommandTest.php
+++ src/Wallabag/CoreBundle/Tests/Command/InstallCommandTest.php
@@ -185,13 +185,7 @@ class InstallCommandTest extends WallabagCoreTestCase
     {
         if ($this->getClient()->getContainer()->get('doctrine')->getConnection()->getDriver() instanceof \Doctrine\DBAL\Driver\PDOPgSql\Driver) {
             /*
-             * LOG:  statement: CREATE DATABASE "wallabag"
-             * ERROR:  source database "template1" is being accessed by other users
-             * DETAIL:  There is 1 other session using the database.
-             * STATEMENT:  CREATE DATABASE "wallabag"
-             * FATAL:  database "wallabag" does not exist
-             *
-             * http://stackoverflow.com/a/14374832/569101
+             * @see testRunInstallCommandWithDatabaseRemoved
              */
             $this->markTestSkipped('PostgreSQL spotted: can find a good way to drop current database, skipping.');
         }
