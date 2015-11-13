f27aca26f645b16eea4a33691431aeaa7d4c1ffe
Kévin Gomez <contact@kevingomez.fr>
2015-11-13 20:48:51 +0100

    Add phpdoc for all Matches implementations

--- src/Wallabag/CoreBundle/Operator/Doctrine/Matches.php
+++ src/Wallabag/CoreBundle/Operator/Doctrine/Matches.php
@@ -2,6 +2,16 @@
 
 namespace Wallabag\CoreBundle\Operator\Doctrine;
 
+/**
+ * Provides a "matches" operator used for tagging rules.
+ *
+ * It asserts that a given pattern is contained in a subject, in a
+ * case-insensitive way.
+ *
+ * This operator will be used to compile tagging rules in DQL, usable
+ * by Doctrine ORM.
+ * It's registered in RulerZ using a service (wallabag.operator.doctrine.matches);
+ */
 class Matches
 {
     public function __invoke($subject, $pattern)
--- src/Wallabag/CoreBundle/Operator/PHP/Matches.php
+++ src/Wallabag/CoreBundle/Operator/PHP/Matches.php
@@ -2,6 +2,16 @@
 
 namespace Wallabag\CoreBundle\Operator\PHP;
 
+/**
+ * Provides a "matches" operator used for tagging rules.
+ *
+ * It asserts that a given pattern is contained in a subject, in a
+ * case-insensitive way.
+ *
+ * This operator will be used to compile tagging rules in PHP, usable
+ * directly on Entry objects for instance.
+ * It's registered in RulerZ using a service (wallabag.operator.array.matches);
+ */
 class Matches
 {
     public function __invoke($subject, $pattern)
