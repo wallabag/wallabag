a3cc0dd0bfedf8d77c232eb38e8781b3cf38a048
Jeremy Benoist <j0k3r@users.noreply.github.com>
2016-10-03 18:13:40 +0200

    Add missing env in install.sh
    Fix https://github.com/wallabag/wallabag/issues/2345

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 echo " > Installing PHP dependencies through Composer..."
-composer install --no-interaction --no-progress --prefer-dist -o --no-dev
+SYMFONY_ENV=prod composer install --no-interaction --no-progress --prefer-dist -o --no-dev
 
 chmod ugo+x vendor/mouf/nodejs-installer/bin/local/npm
 echo " > Downloading librairies through npm..."
