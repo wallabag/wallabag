4ac780eb21f1cceeca34e698e6865d49ec7d3ee8
Jeremy Benoist <jeremy.benoist@gmail.com>
2016-10-03 14:33:05 +0200

    Use install_dev.sh for Travis
    Add `--no-dev` to `composer install` for prod env

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #! /usr/bin/env bash
 
-if [[ $ASSETS == 'build' ]]; then
-    echo "Installing PHP dependencies through Composer..."
-    composer install --no-interaction --no-progress --prefer-dist -o
+echo " > Installing PHP dependencies through Composer..."
+composer install --no-interaction --no-progress --prefer-dist -o --no-dev
 
-    chmod ugo+x vendor/mouf/nodejs-installer/bin/local/npm
-    echo "Downloading librairies through npm..."
-    vendor/mouf/nodejs-installer/bin/local/npm install
+chmod ugo+x vendor/mouf/nodejs-installer/bin/local/npm
+echo " > Downloading librairies through npm..."
+vendor/mouf/nodejs-installer/bin/local/npm install
 
-    echo "Concat, minify and installing assets..."
-    node_modules/grunt/bin/grunt
-else
-    composer install --no-interaction --no-progress --prefer-dist -o
-fi
+echo " > Concat, minify and installing assets..."
+node_modules/grunt/bin/grunt
+
+echo " > Install finished"
