7d173a28c784b647b4c4fb1857aa7c21da5961c3
Darius Kramer <dev@dkramer.fastmail.com>
2016-11-07 09:55:03 +0100

    Reorder variable assignation in update.sh script, fix #2554

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,11 +9,11 @@ if [ ! -d "$DIR" ]; then DIR="$PWD/scripts"; fi
 . "$DIR/require.sh"
 
 ENV=$1
-TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 rm -rf var/cache/*
 git fetch origin
 git fetch --tags
+TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 git checkout $TAG --force
 SYMFONY_ENV=$ENV $COMPOSER_COMMAND install --no-dev -o --prefer-dist
 php bin/console cache:clear --env=$ENV
