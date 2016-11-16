94f2057ffefdb164ae388a6971b4ed052082a434
Nicolas Lœuillet <nicolas@loeuillet.org>
2016-11-16 09:17:12 +0100

    Added CSS class on links

--- app/Resources/static/themes/material/js/shortcuts/entry.js
+++ app/Resources/static/themes/material/js/shortcuts/entry.js
@@ -3,20 +3,20 @@ import $ from 'jquery';
 
 /* open original article */
 Mousetrap.bind('o', () => {
-  $('ul.side-nav li:nth-child(2) a i')[0].click();
+  $('ul.side-nav a.original i')[0].click();
 });
 
 /* mark as favorite */
 Mousetrap.bind('s', () => {
-  $('ul.side-nav li:nth-child(5) a i')[0].click();
+  $('ul.side-nav a.favorite i')[0].click();
 });
 
 /* mark as read */
 Mousetrap.bind('a', () => {
-  $('ul.side-nav li:nth-child(4) a i')[0].click();
+  $('ul.side-nav a.markasread i')[0].click();
 });
 
 /* delete */
 Mousetrap.bind('del', () => {
-  $('ul.side-nav li:nth-child(6) a i')[0].click();
+  $('ul.side-nav a.delete i')[0].click();
 });
