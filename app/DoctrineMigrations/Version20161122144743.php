d64bf7953b0e4e793d7f75ec50bf6e42652560b2
Nicolas Lœuillet <nicolas@loeuillet.org>
2016-11-22 14:56:53 +0100

    Added internal setting to enable/disable articles with paywall

---
+++ app/DoctrineMigrations/Version20161122144743.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Add the restricted_access internal setting for articles with paywall
+ */
+class Version20161122144743 extends AbstractMigration implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    private function getTable($tableName)
+    {
+        return $this->container->getParameter('database_table_prefix') . $tableName;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql("INSERT INTO ".$this->getTable('craue_config_setting')." (name, value, section) VALUES ('restricted_access', 0, 'entry')");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql("DELETE FROM ".$this->getTable('craue_config_setting')." WHERE name = 'restricted_access';");
+    }
+}
--- src/Wallabag/CoreBundle/Helper/HttpClientFactory.php
+++ src/Wallabag/CoreBundle/Helper/HttpClientFactory.php
@@ -18,23 +18,31 @@ class HttpClientFactory
     /** @var \GuzzleHttp\Cookie\CookieJar */
     private $cookieJar;
 
+    private $restrictedAccess;
+
     /**
      * HttpClientFactory constructor.
      *
      * @param \GuzzleHttp\Event\SubscriberInterface $authenticatorSubscriber
      * @param \GuzzleHttp\Cookie\CookieJar          $cookieJar
+     * @param string                                $restrictedAccess
      */
-    public function __construct(SubscriberInterface $authenticatorSubscriber, CookieJar $cookieJar)
+    public function __construct(SubscriberInterface $authenticatorSubscriber, CookieJar $cookieJar, $restrictedAccess)
     {
         $this->authenticatorSubscriber = $authenticatorSubscriber;
         $this->cookieJar = $cookieJar;
+        $this->restrictedAccess = $restrictedAccess;
     }
 
     /**
-     * @return \GuzzleHttp\Client
+     * @return \GuzzleHttp\Client|null
      */
     public function buildHttpClient()
     {
+        if (0 === (int) $this->restrictedAccess) {
+            return null;
+        }
+
         // we clear the cookie to avoid websites who use cookies for analytics
         $this->cookieJar->clear();
         // need to set the (shared) cookie jar
