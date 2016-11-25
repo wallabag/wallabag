73f7eabb6e10cff09a79105b6525e3c269e3cf08
Nicolas Lœuillet <nicolas@loeuillet.org>
2016-11-25 13:47:09 +0100

    Added hasColumn() in migration to check column existence

--- app/DoctrineMigrations/Version20160410190541.php
+++ app/DoctrineMigrations/Version20160410190541.php
@@ -21,7 +21,41 @@ class Version20160410190541 extends AbstractMigration implements ContainerAwareI
 
     private function getTable($tableName)
     {
-        return $this->container->getParameter('database_table_prefix') . $tableName;
+        return $this->container->getParameter('database_table_prefix').$tableName;
+    }
+
+    private function hasColumn($tableName, $columnName)
+    {
+        switch ($this->connection->getDatabasePlatform()->getName()) {
+            case 'sqlite':
+                $rows = $this->connection->executeQuery('pragma table_info('.$tableName.')')->fetchAll();
+                foreach ($rows as $column) {
+                    if (strcasecmp($column['name'], $columnName) === 0) {
+                        return true;
+                    }
+                }
+
+                return false;
+            case 'mysql':
+                $rows = $this->connection->executeQuery('SHOW COLUMNS FROM '.$tableName)->fetchAll();
+                foreach ($rows as $column) {
+                    if (strcasecmp($column['Field'], $columnName) === 0) {
+                        return true;
+                    }
+                }
+
+                return false;
+            case 'postgresql':
+                $sql = sprintf("SELECT count(*)
+                    FROM information_schema.columns
+                    WHERE table_schema = 'public' AND table_name = '%s' AND column_name = '%s'",
+                    $tableName,
+                    $columnName
+                );
+                $result = $this->connection->executeQuery($sql)->fetch();
+
+                return  $result['count'] > 0;
+        }
     }
 
     /**
@@ -29,13 +63,15 @@ class Version20160410190541 extends AbstractMigration implements ContainerAwareI
      */
     public function up(Schema $schema)
     {
+        $this->skipIf($this->hasColumn($this->getTable('entry'), 'uuid'), 'It seems that you already played this migration.');
+
         if ($this->connection->getDatabasePlatform()->getName() == 'postgresql') {
             $this->addSql('ALTER TABLE "'.$this->getTable('entry').'" ADD uuid UUID DEFAULT NULL');
         } else {
             $this->addSql('ALTER TABLE "'.$this->getTable('entry').'" ADD uuid LONGTEXT DEFAULT NULL');
         }
 
-        $this->addSql("INSERT INTO \"".$this->getTable('craue_config_setting')."\" (name, value, section) VALUES ('share_public', '1', 'entry')");
+        $this->addSql('INSERT INTO "'.$this->getTable('craue_config_setting')."\" (name, value, section) VALUES ('share_public', '1', 'entry')");
     }
 
     /**
@@ -43,9 +79,9 @@ class Version20160410190541 extends AbstractMigration implements ContainerAwareI
      */
     public function down(Schema $schema)
     {
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'sqlite', 'This down migration can\'t be executed on SQLite databases, because SQLite don\'t support DROP COLUMN.');
+        $this->skipIf($this->connection->getDatabasePlatform()->getName() != 'sqlite', 'This down migration can\'t be executed on SQLite databases, because SQLite don\'t support DROP COLUMN.');
 
         $this->addSql('ALTER TABLE "'.$this->getTable('entry').'" DROP uuid');
-        $this->addSql("DELETE FROM \"".$this->getTable('craue_config_setting')."\" WHERE name = 'share_public'");
+        $this->addSql('DELETE FROM "'.$this->getTable('craue_config_setting')."\" WHERE name = 'share_public'");
     }
 }
