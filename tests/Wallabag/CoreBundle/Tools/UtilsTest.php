5b6888b13fd44705a232261862a5cc96cebd0a7d
Andrew Kovalyov <andrew.kovalyoff@gmail.com>
2016-11-28 01:15:06 +0200

    Fix incorrect reading time calculation for entries in languages with non-latin chars subset.

---
+++ tests/Wallabag/CoreBundle/Tools/UtilsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Wallabag\CoreBundle\Tools;
+
+use Symfony\Component\Finder\Finder;
+use Wallabag\CoreBundle\Tools\Utils;
+
+class UtilsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider examples
+     */
+    public function testCorrectWordsCountForDifferentLanguages($text, $expectedCount)
+    {
+        static::assertEquals((float) $expectedCount, Utils::getReadingTime($text));
+    }
+
+    public function examples()
+    {
+        $examples = [];
+        $finder = (new Finder())->in(__DIR__.'/samples');
+        foreach ($finder->getIterator() as $file) {
+            $examples[] = [$file->getContents(), 1];
+        }
+
+        return $examples;
+    }
+}
