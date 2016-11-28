07326af5e25f52f54f725898d9fb2f82af60e224
Nicolas Lœuillet <nicolas@loeuillet.org>
2016-11-28 13:25:18 +0100

    Added migration to remove useless fields

---
+++ app/DoctrineMigrations/Version20161128131503.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Removed locked, credentials_expire_at and expires_at.
+ */
+class Version20161128131503 extends AbstractMigration implements ContainerAwareInterface
+{
+    private $fields = [
+        'locked' => 'smallint',
+        'credentials_expire_at' => 'datetime',
+        'expires_at' => 'datetime',
+    ];
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    private function getTable($tableName)
+    {
+        return $this->container->getParameter('database_table_prefix').$tableName;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $userTable = $schema->getTable($this->getTable('user'));
+
+        foreach ($this->fields as $field => $type) {
+            $this->skipIf(!$userTable->hasColumn($field), 'It seems that you already played this migration.');
+            $userTable->dropColumn($field);
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $userTable = $schema->getTable($this->getTable('user'));
+
+        foreach ($this->fields as $field => $type) {
+            $this->skipIf($userTable->hasColumn($field), 'It seems that you already played this migration.');
+            $userTable->addColumn($field, $type);
+        }
+    }
+}
