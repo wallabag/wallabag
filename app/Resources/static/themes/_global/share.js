77255d668828d7ae4cceb186b5215a0d4ddd69f6
Thomas Citharel <tcit@tcit.fr>
2017-06-22 12:15:24 +0200

    Add css on share public page

---
+++ app/Resources/static/themes/_global/share.js
@@ -0,0 +1,1 @@
+import './share.scss';
--- app/config/webpack/common.js
+++ app/config/webpack/common.js
@@ -9,6 +9,7 @@ module.exports = function () {
     entry: {
       material: path.join(rootDir, './app/Resources/static/themes/material/index.js'),
       baggy: path.join(rootDir, './app/Resources/static/themes/baggy/index.js'),
+      public: path.join(rootDir, './app/Resources/static/themes/_global/share.js'),
     },
     output: {
       filename: '[name].js',
