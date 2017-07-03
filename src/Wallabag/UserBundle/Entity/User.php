e50d7d31c0746ffbfe69065258981e1b4dcd203b
Jeremy Benoist <jeremy.benoist@gmail.com>
2017-07-03 11:49:46 +0200

    CS

--- src/Wallabag/UserBundle/Entity/User.php
+++ src/Wallabag/UserBundle/Entity/User.php
@@ -97,7 +97,7 @@ class User extends BaseUser implements TwoFactorInterface, TrustedComputerInterf
      *
      * @ORM\OneToMany(targetEntity="Wallabag\CoreBundle\Entity\SiteCredential", mappedBy="user", cascade={"remove"})
      */
-    protected $site_credentials;
+    protected $siteCredentials;
 
     /**
      * @var ArrayCollection
