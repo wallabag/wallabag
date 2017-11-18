c8ac32cf171b60e8e496bbaf781cbf309bd1e04c
Jeremy Benoist <jeremy.benoist@gmail.com>
2017-11-18 16:28:54 +0100

    Jump to Simpleue 2.0
    The 1.3.0 tag is introducing BC so we jump to the 2.0

--- src/Wallabag/ImportBundle/Consumer/RedisEntryConsumer.php
+++ src/Wallabag/ImportBundle/Consumer/RedisEntryConsumer.php
@@ -21,9 +21,26 @@ class RedisEntryConsumer extends AbstractConsumer implements Job
     /**
      * Should tell if the given job will kill the worker.
      * We don't want to stop it :).
+     *
+     * @param string $job Content of the message (directly from Redis)
+     *
+     * @return false
      */
     public function isStopJob($job)
     {
         return false;
+    }
+
+    /**
+     * This abstract method is only used when we use one queue for multiple job type.
+     * We don't do that, so we'll always return true.
+     *
+     * @param string $job Content of the message (directly from Redis)
+     *
+     * @return true
+     */
+    public function isMyJob($job)
+    {
+        return true;
     }
 }
