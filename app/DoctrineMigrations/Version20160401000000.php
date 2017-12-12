3fadf42a1c177fc41748c7732cbb4b13cbee4a14
Jeremy Benoist <jeremy.benoist@gmail.com>
2017-12-12 14:30:22 +0100

    CS

--- app/DoctrineMigrations/Version20160401000000.php
+++ app/DoctrineMigrations/Version20160401000000.php
@@ -4,7 +4,6 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Schema\SchemaException;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
