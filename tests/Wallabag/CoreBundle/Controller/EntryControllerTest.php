26e2f07419c9a16c2a304baa6d2cd36d6dddcd80
Jeremy Benoist <jeremy.benoist@gmail.com>
2017-12-16 22:17:56 +0100

    Fix typo

--- tests/Wallabag/CoreBundle/Controller/EntryControllerTest.php
+++ tests/Wallabag/CoreBundle/Controller/EntryControllerTest.php
@@ -10,7 +10,7 @@ use Wallabag\CoreBundle\Helper\ContentProxy;
 
 class EntryControllerTest extends WallabagCoreTestCase
 {
-    const An_URL_CONTAINING_AN_ARTICLE_WITH_IMAGE = 'http://www.lemonde.fr/judo/article/2017/11/11/judo-la-decima-de-teddy-riner_5213605_1556020.html';
+    const AN_URL_CONTAINING_AN_ARTICLE_WITH_IMAGE = 'http://www.lemonde.fr/judo/article/2017/11/11/judo-la-decima-de-teddy-riner_5213605_1556020.html';
     public $downloadImagesEnabled = false;
     public $url = 'http://www.lemonde.fr/pixels/article/2015/03/28/plongee-dans-l-univers-d-ingress-le-jeu-de-google-aux-frontieres-du-reel_4601155_4408996.html';
 
@@ -1018,7 +1018,7 @@ class EntryControllerTest extends WallabagCoreTestCase
         $this->logInAs('admin');
         $client = $this->getClient();
 
-        $url = self::An_URL_CONTAINING_AN_ARTICLE_WITH_IMAGE;
+        $url = self::AN_URL_CONTAINING_AN_ARTICLE_WITH_IMAGE;
         $client->getContainer()->get('craue_config')->set('download_images_enabled', 1);
 
         $crawler = $client->request('GET', '/new');
@@ -1060,7 +1060,7 @@ class EntryControllerTest extends WallabagCoreTestCase
         $this->logInAs('admin');
         $client = $this->getClient();
 
-        $url = self::An_URL_CONTAINING_AN_ARTICLE_WITH_IMAGE;
+        $url = self::AN_URL_CONTAINING_AN_ARTICLE_WITH_IMAGE;
         $client->getContainer()->get('craue_config')->set('download_images_enabled', 1);
 
         $crawler = $client->request('GET', '/new');
