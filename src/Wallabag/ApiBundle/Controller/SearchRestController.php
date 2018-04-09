b32057980e33e7ddd93480017496a589006b8260
Craig Roberts <craig@craig0990.co.uk>
2018-04-09 17:24:45 +0800

    Fixes [wallabag/wallabag#2611] Add a basic Search REST endpoint
    - Adds a new `search` key to `src/Wallabag/ApiBundle/Resources/config/routing_rest.yml`
    - Reuses the `getBuilderForSearchByUser` method from the EntryRepository
    - Supports, `term`, `page`, and `perPage` query parameters
    - Some very basic tests

---
+++ src/Wallabag/ApiBundle/Controller/SearchRestController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Wallabag\ApiBundle\Controller;
+
+use Hateoas\Configuration\Route;
+use Hateoas\Representation\Factory\PagerfantaFactory;
+use JMS\Serializer\SerializationContext;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Wallabag\CoreBundle\Entity\Entry;
+use Wallabag\CoreBundle\Entity\Tag;
+use Wallabag\CoreBundle\Event\EntryDeletedEvent;
+use Wallabag\CoreBundle\Event\EntrySavedEvent;
+
+class SearchRestController extends WallabagRestController
+{
+    /**
+     * Search all entries by term.
+     *
+     * @ApiDoc(
+     *       parameters={
+     *          {"name"="term", "dataType"="string", "required"=false, "format"="any", "description"="Any query term"},
+     *          {"name"="page", "dataType"="integer", "required"=false, "format"="default '1'", "description"="what page you want."},
+     *          {"name"="perPage", "dataType"="integer", "required"=false, "format"="default'30'", "description"="results per page."}
+     *       }
+     * )
+     *
+     * @return JsonResponse
+     */
+    public function getSearchAction(Request $request)
+    {
+        $this->validateAuthentication();
+
+        $term = $request->query->get('term');
+        $page = (int) $request->query->get('page', 1);
+        $perPage = (int) $request->query->get('perPage', 30);
+
+        $qb = $this->get('wallabag_core.entry_repository')
+            ->getBuilderForSearchByUser(
+                $this->getUser()->getId(),
+                $term,
+                null
+            );
+
+        $pagerAdapter = new DoctrineORMAdapter($qb->getQuery(), true, false);
+        $pager = new Pagerfanta($pagerAdapter);
+
+        $pager->setMaxPerPage($perPage);
+        $pager->setCurrentPage($page);
+
+        $pagerfantaFactory = new PagerfantaFactory('page', 'perPage');
+        $paginatedCollection = $pagerfantaFactory->createRepresentation(
+            $pager,
+            new Route(
+                'api_get_search',
+                [
+                    'term' => $term,
+                    'page' => $page,
+                    'perPage' => $perPage,
+                ],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            )
+        );
+
+        return $this->sendResponse($paginatedCollection);
+    }
+
+    /**
+     * Shortcut to send data serialized in json.
+     *
+     * @param mixed $data
+     *
+     * @return JsonResponse
+     */
+    private function sendResponse($data)
+    {
+        // https://github.com/schmittjoh/JMSSerializerBundle/issues/293
+        $context = new SerializationContext();
+        $context->setSerializeNull(true);
+
+        $json = $this->get('jms_serializer')->serialize($data, 'json', $context);
+
+        return (new JsonResponse())->setJson($json);
+    }
+}
