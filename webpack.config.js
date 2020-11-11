9937d3a0601aea04721b3b6de00816bf75da783a
Simounet <contact@simounet.net>
2020-11-11 09:48:04 +0100

    Webpack upgraded from 4 to 5 + dependancies updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
-function buildConfig(env) {
-  env = env || 'prod';
-  return require(path.resolve(__dirname, 'app/config/webpack/' + env + '.js'))({ env: env })
+function buildConfig(options) {
+  const env = options.prod ? 'prod' : 'dev';
+  return require(path.resolve(__dirname, `app/config/webpack/${env}.js`));
 }
 
 module.exports = buildConfig;
