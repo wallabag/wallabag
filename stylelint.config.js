5f8948d5a122aecc2052e2fbfaa7551f035af4fe
Simounet <contact@simounet.net>
2023-06-06 22:43:34 +0200

    stylelint-config-standard-scss added for stylelint SCSS handling

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-standard-scss'],
   customSyntax: require('postcss-scss'),
   plugins: ['stylelint-scss'],
   rules: {
