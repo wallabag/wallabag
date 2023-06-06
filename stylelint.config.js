d7222bf4db3fcb6bb6561bb69971517135124184
Simounet <contact@simounet.net>
2023-06-06 23:26:45 +0200

    [Boyscout] Stylelint: uneeded require removed

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-standard-scss'],
-  customSyntax: require('postcss-scss'),
+  customSyntax: 'postcss-scss',
   plugins: ['stylelint-scss'],
   rules: {
     'at-rule-no-unknown': null,
