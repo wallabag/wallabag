2a2f5b8359e52ec019d60bcc3fe9e32819332bd0
Yassine Guedidi <yassine@guedidi.com>
2023-08-08 16:00:39 +0100

    Rename share.js to public.js

--- app/config/webpack/common.js
+++ app/config/webpack/common.js
@@ -7,7 +7,7 @@ const rootDir = path.resolve(__dirname, '../../../');
 module.exports = {
   entry: {
     material: path.join(rootDir, './app/Resources/static/themes/material/index.js'),
-    public: path.join(rootDir, './app/Resources/static/share.js'),
+    public: path.join(rootDir, './app/Resources/static/public.js'),
   },
   output: {
     filename: '[name].js',
