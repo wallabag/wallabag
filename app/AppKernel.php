f26124776612702a5f0bbbb9857beffd781ca54c
Yassine Guedidi <yassine@guedidi.com>
2023-08-10 01:10:54 +0100

    Use Webpack Encore

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             new Http\HttplugBundle\HttplugBundle(),
             new Sentry\SentryBundle\SentryBundle(),
             new Twig\Extra\TwigExtraBundle\TwigExtraBundle(),
+            new Symfony\WebpackEncoreBundle\WebpackEncoreBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,36 @@
-const path = require('path');
+const Encore = require('@symfony/webpack-encore');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-function buildConfig(options) {
-  const env = options.prod ? 'prod' : 'dev';
-  return require(path.resolve(__dirname, `app/config/webpack/${env}.js`));
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-module.exports = buildConfig;
+Encore
+  .setOutputPath('web/build/')
+  .setPublicPath('/build')
+  .addEntry('main', './assets/index.js')
+  .addEntry('public', './assets/share.js')
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .configureBabelPresetEnv((config) => {
+    config.modules = false;
+    config.useBuiltIns = 'usage';
+    config.corejs = '3.23';
+  })
+  .enableSassLoader()
+  .enablePostCssLoader()
+  .autoProvidejQuery()
+  .enableEslintPlugin()
+  .addPlugin(new StyleLintPlugin({
+    configFile: 'stylelint.config.js',
+    failOnError: false,
+    quiet: false,
+    context: 'assets',
+    files: '**/*.scss',
+  }));
+
+module.exports = Encore.getWebpackConfig();
