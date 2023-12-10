23ea7b35e629c8e7a8a84d9f2124120730ea6249
Yassine Guedidi <yassine@guedidi.com>
2023-12-10 21:36:17 +0100

    Sync remaining files with framework-bundle recipe

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
