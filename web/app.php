5ca2a70db01a53385f00bd1a26a9e50ce4aa3fc0
Yassine Guedidi <yassine@guedidi.com>
2023-12-12 00:21:49 +0100

    Make app.php and app_dev.php use index.php

--- web/app.php
+++ web/app.php
@@ -1,15 +1,6 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
+putenv('APP_ENV=prod');
+putenv('APP_DEBUG=0');
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$kernel = new AppKernel('prod', false);
-//$kernel = new CacheKernel($kernel);
-
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return require __DIR__ . '/../public/index.php';
--- web/app_dev.php
+++ web/app_dev.php
@@ -1,29 +1,6 @@
 <?php
 
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
-use Wallabag\Kernel;
+putenv('APP_ENV=dev');
+putenv('APP_DEBUG=1');
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
-
-require __DIR__.'/../vendor/autoload.php';
-Debug::enable();
-
-$kernel = new Kernel('dev', true);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return require __DIR__ . '/../public/index.php';
