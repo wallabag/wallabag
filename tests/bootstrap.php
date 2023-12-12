73398378088e0823dc5f289017154a2a897b4bda
Yassine Guedidi <yassine@guedidi.com>
2023-12-12 21:45:42 +0100

    fixup! Sync with phpunit-bridge recipe

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,11 +6,7 @@ use Symfony\Component\Process\Process;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
-    require dirname(__DIR__) . '/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
