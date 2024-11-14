68beee74035299d30692751d27c707ca5a4d4331
Nicolas Lœuillet <nicolas.loeuillet@prestashop.com>
2024-11-14 18:14:02 +0100

    Add assets build during install and update

--- scripts/install.sh
+++ scripts/install.sh
@@ -39,3 +39,5 @@ TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 git checkout $TAG
 SYMFONY_ENV=$ENV $COMPOSER_COMMAND install --no-dev -o --prefer-dist
 php bin/console wallabag:install --env=$ENV
+yarn install
+yarn build:$ENV
