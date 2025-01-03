3ca3a8b8421575982a00714e5c7eebaa38811458
Adrien ERAUD <e-adrien@users.noreply.github.com>
2025-01-03 11:51:44 +0100

    Requested change + Test

--- src/SiteConfig/LoginFormAuthenticator.php
+++ src/SiteConfig/LoginFormAuthenticator.php
@@ -29,10 +29,12 @@ class LoginFormAuthenticator
             $siteConfig->getPasswordField() => $siteConfig->getPassword(),
         ] + $this->getExtraFields($siteConfig);
 
-        $guzzle->post(
-            $siteConfig->getLoginUri(),
-            ['body' => $postFields, 'allow_redirects' => true, 'verify' => false] + ($siteConfig->hasHttpHeaders() ? ['headers' => $siteConfig->getHttpHeaders()] : []),
-        );
+        $params = ['body' => $postFields, 'allow_redirects' => true, 'verify' => false];
+        if ($siteConfig->hasHttpHeaders()) {
+            $params['headers'] = $siteConfig->getHttpHeaders();
+        }
+
+        $guzzle->post($siteConfig->getLoginUri(), $params);
 
         return $this;
     }
