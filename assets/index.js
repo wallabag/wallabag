4cc8d0c6d09343f280a525f8acbcf622e3923177
Yassine Guedidi <yassine@guedidi.com>
2025-02-23 22:31:10 +0100

    Extract Materialize controller

--- assets/index.js
+++ assets/index.js
@@ -274,24 +274,6 @@ const articleScroll = () => {
 };
 
 $(document).ready(() => {
-  // sidenav
-  $('.sidenav').sidenav();
-  $('select').formSelect();
-  $('.collapsible[data-collapsible="accordion"]').collapsible();
-  $('.collapsible[data-collapsible="expandable"]').collapsible({
-    accordion: false,
-  });
-
-  $('.dropdown-trigger').dropdown({ hover: false });
-  $('.dropdown-trigger[data-covertrigger="false"][data-constrainwidth="false"]').dropdown({
-    hover: false,
-    coverTrigger: false,
-    constrainWidth: false,
-  });
-
-  $('.tabs').tabs();
-  $('.tooltipped').tooltip();
-
   initFilters();
   initExport();
   initRandom();
