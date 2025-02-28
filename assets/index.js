a69ea4694579db1620bca28e773227e97da5e59b
Yassine Guedidi <yassine@guedidi.com>
2025-03-01 00:35:24 +0100

    Extract Materialize Collapsible controller

--- assets/index.js
+++ assets/index.js
@@ -273,10 +273,6 @@ const articleScroll = () => {
 
 $(document).ready(() => {
   $('select').formSelect();
-  $('.collapsible[data-collapsible="accordion"]').collapsible();
-  $('.collapsible[data-collapsible="expandable"]').collapsible({
-    accordion: false,
-  });
 
   $('.dropdown-trigger').dropdown({ hover: false });
   $('.dropdown-trigger[data-covertrigger="false"][data-constrainwidth="false"]').dropdown({
