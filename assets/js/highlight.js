503b82ea1387e533bbe9e415ea96c285b052d23d
Yassine Guedidi <yassine@guedidi.com>
2025-03-01 01:33:48 +0100

    Extract Highlight controller

--- assets/js/highlight.js
+++
@@ -1,8 +0,0 @@
-import 'highlight.js/styles/atom-one-light.css';
-import hljs from 'highlight.js';
-
-window.addEventListener('load', () => {
-  document.querySelectorAll('pre').forEach((element) => {
-    hljs.highlightElement(element);
-  });
-});
