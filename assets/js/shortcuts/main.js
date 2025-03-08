fad90b523e828c36f63fa95ca5e11d87ac779883
Yassine Guedidi <yassine@guedidi.com>
2025-03-09 00:13:58 +0100

    Extract Shortcuts controller

--- assets/js/shortcuts/main.js
+++ assets/js/shortcuts/main.js
@@ -1,18 +1,6 @@
 import Mousetrap from 'mousetrap';
 import $ from 'jquery';
 
-/* Go to */
-Mousetrap.bind('g u', () => { window.location.href = Routing.generate('homepage'); });
-Mousetrap.bind('g s', () => { window.location.href = Routing.generate('starred'); });
-Mousetrap.bind('g r', () => { window.location.href = Routing.generate('archive'); });
-Mousetrap.bind('g a', () => { window.location.href = Routing.generate('all'); });
-Mousetrap.bind('g t', () => { window.location.href = Routing.generate('tag'); });
-Mousetrap.bind('g c', () => { window.location.href = Routing.generate('config'); });
-Mousetrap.bind('g i', () => { window.location.href = Routing.generate('import'); });
-Mousetrap.bind('g d', () => { window.location.href = Routing.generate('developer'); });
-Mousetrap.bind('?', () => { window.location.href = Routing.generate('howto'); });
-Mousetrap.bind('g l', () => { window.location.href = Routing.generate('fos_user_security_logout'); });
-
 function toggleFocus(cardToToogleFocus) {
   if (cardToToogleFocus) {
     $(cardToToogleFocus).toggleClass('z-depth-4');
@@ -47,21 +35,6 @@ $(document).ready(() => {
 
   /* Focus current card */
   toggleFocus(card);
-
-  /* Actions */
-  Mousetrap.bind('g n', () => {
-    $('#nav-btn-add').trigger('click');
-    return false;
-  });
-
-  Mousetrap.bind('s', () => {
-    $('#nav-btn-search').trigger('click');
-    return false;
-  });
-
-  Mousetrap.bind('esc', () => {
-    $('.close').trigger('click');
-  });
 
   /* Select right card. If there's a next page, go to next page */
   Mousetrap.bind('right', () => {
