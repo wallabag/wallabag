787a812f8ed26c007543029c699ccd06f760e201
Yassine Guedidi <yassine@guedidi.com>
2025-03-16 22:13:12 +0100

    Fix tests namespaces

--- tests/Security/Voter/SiteCredentialVoterTest.php
+++ tests/Security/Voter/SiteCredentialVoterTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Security\Voter;
+namespace Tests\Wallabag\Security\Voter;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
