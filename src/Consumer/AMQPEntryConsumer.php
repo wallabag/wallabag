4ab26a19021ea4694794f1b5883496cc33331e41
Yassine Guedidi <yassine@guedidi.com>
2025-04-02 00:42:31 +0200

    Fix self deprecation notices

--- src/Consumer/AMQPEntryConsumer.php
+++ src/Consumer/AMQPEntryConsumer.php
@@ -7,7 +7,7 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class AMQPEntryConsumer extends AbstractConsumer implements ConsumerInterface
 {
-    public function execute(AMQPMessage $msg)
+    public function execute(AMQPMessage $msg): int|bool
     {
         return $this->handleMessage($msg->getBody());
     }
