6fa552c5a8bbcd711d6d7d52216d520a9a33350a
Nicolas Lœuillet <nicolas.loeuillet@prestashop.com>
2025-05-24 17:51:45 +0200

    Add Pocket CSV import

--- tests/Wallabag/ImportBundle/Controller/PocketCsvControllerTest.php
+++ tests/Wallabag/ImportBundle/Controller/PocketCsvControllerTest.php
@@ -9,28 +9,28 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Tests\Wallabag\CoreBundle\WallabagCoreTestCase;
 use Wallabag\CoreBundle\Entity\Entry;
 
-class PocketHtmlControllerTest extends WallabagCoreTestCase
+class PocketCsvControllerTest extends WallabagCoreTestCase
 {
-    public function testImportPocketHtml()
+    public function testImportPocketCsv()
     {
         $this->logInAs('admin');
         $client = $this->getTestClient();
 
-        $crawler = $client->request('GET', '/import/pocket_html');
+        $crawler = $client->request('GET', '/import/pocket_csv');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
         $this->assertSame(1, $crawler->filter('form[name=upload_import_file] > button[type=submit]')->count());
         $this->assertSame(1, $crawler->filter('input[type=file]')->count());
     }
 
-    public function testImportPocketHtmlWithRabbitEnabled()
+    public function testImportPocketCsvWithRabbitEnabled()
     {
         $this->logInAs('admin');
         $client = $this->getTestClient();
 
         $client->getContainer()->get(Config::class)->set('import_with_rabbitmq', 1);
 
-        $crawler = $client->request('GET', '/import/pocket_html');
+        $crawler = $client->request('GET', '/import/pocket_csv');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
         $this->assertSame(1, $crawler->filter('form[name=upload_import_file] > button[type=submit]')->count());
@@ -39,12 +39,12 @@ class PocketHtmlControllerTest extends WallabagCoreTestCase
         $client->getContainer()->get(Config::class)->set('import_with_rabbitmq', 0);
     }
 
-    public function testImportPocketHtmlBadFile()
+    public function testImportPocketCsvBadFile()
     {
         $this->logInAs('admin');
         $client = $this->getTestClient();
 
-        $crawler = $client->request('GET', '/import/pocket_html');
+        $crawler = $client->request('GET', '/import/pocket_csv');
         $form = $crawler->filter('form[name=upload_import_file] > button[type=submit]')->form();
 
         $data = [
@@ -56,14 +56,14 @@ class PocketHtmlControllerTest extends WallabagCoreTestCase
         $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
 
-    public function testImportPocketHtmlWithRedisEnabled()
+    public function testImportPocketCsvWithRedisEnabled()
     {
         $this->checkRedis();
         $this->logInAs('admin');
         $client = $this->getTestClient();
         $client->getContainer()->get(Config::class)->set('import_with_redis', 1);
 
-        $crawler = $client->request('GET', '/import/pocket_html');
+        $crawler = $client->request('GET', '/import/pocket_csv');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
         $this->assertSame(1, $crawler->filter('form[name=upload_import_file] > button[type=submit]')->count());
@@ -71,7 +71,7 @@ class PocketHtmlControllerTest extends WallabagCoreTestCase
 
         $form = $crawler->filter('form[name=upload_import_file] > button[type=submit]')->form();
 
-        $file = new UploadedFile(__DIR__ . '/../fixtures/ril_export.html', 'Bookmarks');
+        $file = new UploadedFile(__DIR__ . '/../fixtures/pocket.csv', 'Bookmarks');
 
         $data = [
             'upload_import_file[file]' => $file,
@@ -86,20 +86,20 @@ class PocketHtmlControllerTest extends WallabagCoreTestCase
         $this->assertGreaterThan(1, $body = $crawler->filter('body')->extract(['_text']));
         $this->assertStringContainsString('flashes.import.notice.summary', $body[0]);
 
-        $this->assertNotEmpty($client->getContainer()->get(Client::class)->lpop('wallabag.import.pocket_html'));
+        $this->assertNotEmpty($client->getContainer()->get(Client::class)->lpop('wallabag.import.pocket_csv'));
 
         $client->getContainer()->get(Config::class)->set('import_with_redis', 0);
     }
 
-    public function testImportWallabagWithPocketHtmlFile()
+    public function testImportWallabagWithPocketCsvFile()
     {
         $this->logInAs('admin');
         $client = $this->getTestClient();
 
-        $crawler = $client->request('GET', '/import/pocket_html');
+        $crawler = $client->request('GET', '/import/pocket_csv');
         $form = $crawler->filter('form[name=upload_import_file] > button[type=submit]')->form();
 
-        $file = new UploadedFile(__DIR__ . '/../fixtures/ril_export.html', 'Bookmarks');
+        $file = new UploadedFile(__DIR__ . '/../fixtures/pocket.csv', 'Bookmarks');
 
         $data = [
             'upload_import_file[file]' => $file,
@@ -118,28 +118,15 @@ class PocketHtmlControllerTest extends WallabagCoreTestCase
             ->get(EntityManagerInterface::class)
             ->getRepository(Entry::class)
             ->findByUrlAndUserId(
-                'https://www.20minutes.fr/sport/4002755-20220928-tarn-lapins-ravagent-terrain-match-rugby-doit-etre-annule',
+                'https://jp-lambert.me/est-ce-que-jai-besoin-d-un-scrum-master-604f5a471c73',
                 $this->getLoggedInUserId()
             );
 
         $this->assertInstanceOf(Entry::class, $content);
-        $this->assertNotEmpty($content->getMimetype(), 'Mimetype for 20minutes.fr is ok');
-        $this->assertNotEmpty($content->getPreviewPicture(), 'Preview picture for 20minutes.fr is ok');
-        $this->assertNotEmpty($content->getLanguage(), 'Language for 20minutes.fr is ok');
-        $this->assertCount(3, $content->getTags());
-
-        $content = $client->getContainer()
-            ->get(EntityManagerInterface::class)
-            ->getRepository(Entry::class)
-            ->findByUrlAndUserId(
-                'https://www.20minutes.fr/paris/4100740-20240715-jo-paris-2024-courir-capitale-maintenant-quais-fermes',
-                $this->getLoggedInUserId()
-            );
-
-        $this->assertInstanceOf(Entry::class, $content);
-        $this->assertNotEmpty($content->getMimetype(), 'Mimetype for 20minutes.fr is ok');
-        $this->assertNotEmpty($content->getPreviewPicture(), 'Preview picture for 20minutes.fr is ok');
-        $this->assertNotEmpty($content->getLanguage(), 'Language for 20minutes.fr is ok');
+        $this->assertNotEmpty($content->getMimetype(), 'Mimetype for jp-lambert.me is ok');
+        $this->assertNotEmpty($content->getPreviewPicture(), 'Preview picture for jp-lambert.me is ok');
+        $this->assertNotEmpty($content->getLanguage(), 'Language for jp-lambert.me is ok');
+        $this->assertCount(1, $content->getTags());
     }
 
     public function testImportWallabagWithEmptyFile()
@@ -147,10 +134,10 @@ class PocketHtmlControllerTest extends WallabagCoreTestCase
         $this->logInAs('admin');
         $client = $this->getTestClient();
 
-        $crawler = $client->request('GET', '/import/pocket_html');
+        $crawler = $client->request('GET', '/import/pocket_csv');
         $form = $crawler->filter('form[name=upload_import_file] > button[type=submit]')->form();
 
-        $file = new UploadedFile(__DIR__ . '/../fixtures/test.html', 'test.html');
+        $file = new UploadedFile(__DIR__ . '/../fixtures/test.csv', 'test.csv');
 
         $data = [
             'upload_import_file[file]' => $file,
