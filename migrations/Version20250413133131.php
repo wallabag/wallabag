69399b1c10db23baee4e8ed1477357b3885a5957
Kevin Decherf <kevin@kdecherf.com>
2025-07-24 19:51:02 +0200

    fixup! Add a two-step setup of OTP
    Signed-off-by: Kevin Decherf <kevin@kdecherf.com>

--- migrations/Version20250413133131.php
+++ migrations/Version20250413133131.php
@@ -22,8 +22,21 @@ final class Version20250413133131 extends WallabagMigration
             'default' => false,
             'notnull' => true,
         ]);
+    }
 
-        $this->addSql("UPDATE " . $this->getTable('user') . " SET google_authenticator = 1 WHERE googleAuthenticatorSecret IS NOT NULL AND googleAuthenticatorSecret <> '';");
+    /**
+     * Query to update data in user table, as it's not possible to perform this in the `up` method.
+     */
+    public function postUp(Schema $schema): void
+    {
+        $this->skipIf(!$schema->getTable($this->getTable('user'))->hasColumn('google_authenticator'), 'Unable to update google_authenticator column');
+        $this->connection->executeQuery(
+            'UPDATE ' . $this->getTable('user') . ' SET google_authenticator = :googleAuthenticator WHERE googleAuthenticatorSecret IS NOT NULL AND googleAuthenticatorSecret <> :emptyString',
+            [
+                'googleAuthenticator' => true,
+                'emptyString' => '',
+            ]
+        );
     }
 
     public function down(Schema $schema): void
