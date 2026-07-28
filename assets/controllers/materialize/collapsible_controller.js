65eadbd69dc88d8eac82edbe5a8462b371b95661
Yassine Guedidi <yassine@guedidi.com>
2026-07-29 00:54:39 +0200

    fixup! Expose expanded state for reader controls - synchronize Materialize disclosure state
    Use Materialize disclosure callbacks to reflect the actual expanded state on reader disclosure buttons.
    
    Framework state synchronization belongs beside the disclosure relationships introduced in the same reader-state block.

--- assets/controllers/materialize/collapsible_controller.js
+++ assets/controllers/materialize/collapsible_controller.js
@@ -7,7 +7,11 @@ export default class extends Controller {
   };
 
   connect() {
-    this.instance = M.Collapsible.init(this.element, { accordion: this.accordionValue });
+    this.instance = M.Collapsible.init(this.element, {
+      accordion: this.accordionValue,
+      onOpenStart: (listItem) => this.#setExpanded(listItem, true),
+      onCloseStart: (listItem) => this.#setExpanded(listItem, false),
+    });
     this.buttonHeaders = [...this.element.querySelectorAll('button.collapsible-header')];
     const keydownHandler = this.instance['_handleCollapsibleKeydownBound'];
 
@@ -18,5 +22,11 @@ export default class extends Controller {
 
   disconnect() {
     this.instance.destroy();
+  }
+
+  #setExpanded(listItem, expanded) {
+    const header = listItem.querySelector(':scope > .collapsible-header[aria-expanded]');
+
+    header?.setAttribute('aria-expanded', expanded.toString());
   }
 }
